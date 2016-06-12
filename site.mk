0108e1297d325975dc347cb92830f283359fd089
ffibk <rolf@netzwerkadmins.de>
2016-06-12 09:33:47 +0000

    Add files via upload

---
+++ site.mk
@@ -0,0 +1,34 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-tunneldigger \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-filter-roguenets \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-next-node \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-migrate-vpn \
+	gluon-txpower-fix \
+	iwinfo \
+	iptables \
+	haveged
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
+
+GLUON_LANGS ?= en de
