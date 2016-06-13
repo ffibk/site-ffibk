41397f932cd28298756bab2433db0f613c603757
ffibk <rolf@netzwerkadmins.de>
2016-06-13 17:10:07 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,34 +1,32 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
-	gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
-	gluon-config-mode-tunneldigger \
+	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-ebtables-filter-roguenets \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-node-role \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-mesh-vpn-fastd \
 	gluon-next-node \
-	gluon-mesh-vpn-tunneldigger \
-	gluon-tunneldigger-watchdog \
 	gluon-radvd \
+	gluon-respondd \
 	gluon-setup-mode \
 	gluon-status-page \
-	gluon-migrate-vpn \
-	gluon-txpower-fix \
 	iwinfo \
 	iptables \
 	haveged
 	
-DEFAULT_GLUON_RELEASE := v1.2.X-exp-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := v0.1.1-exp-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
